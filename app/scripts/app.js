633aef1b73673dcbffec171f35920fba1bd8baae
zridi omar <omar.zridi@gmail.com>
2016-03-27 23:30:51 +0200

    first_commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name firstAppApp
+ * @description
+ * # firstAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('firstAppApp', []);
